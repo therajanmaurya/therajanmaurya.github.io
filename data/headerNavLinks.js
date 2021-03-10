2223feee1a4c3ee35f14eed0ec68492ba0d53389
Rajan Maurya <rajanmaurya154@gmail.com>
2021-03-10 20:20:34 +0530

    Basic changes

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -3,6 +3,7 @@ const headerNavLinks = [
   { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
+  { href: '/about', title: 'It\'s Me' },
 ]
 
 export default headerNavLinks
